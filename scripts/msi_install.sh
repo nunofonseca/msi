e1a73b1a2c32e76be30e2a52cc419d39f62f8fea
Nuno Fonseca <firstname.lastname@gmail.com>
2020-06-03 00:58:56 +0100

    New version 0.2.8

--- scripts/msi_install.sh
+++ scripts/msi_install.sh
@@ -309,7 +309,7 @@ function install_all {
 }
 
 function msi_to_docker {
-    MSI_VERSION="0.2.7"
+    MSI_VERSION="0.2.8"
     set -e
     echo "Generating docker image with MSI...this may take a while"
     pushd $PATH2SCRIPT/..
